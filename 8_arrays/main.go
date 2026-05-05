307b22fadc02ace28c52680005ac6564cae3b791
Bedant Chhetri <bedantchhetri7@gmail.com>
2026-05-05 16:10:59 +0000

    add array examples and demonstrate array initialization

---
+++ 8_arrays/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+// numbered sequence of specific length
+func main() {
+	// var nums [4]int
+
+	//array length
+	// fmt.Println(len(nums))
+
+	// nums[0] = 1
+
+	// fmt.Println(nums[0])
+	// fmt.Println(nums)
+
+	// var vals [4]bool
+	// fmt.Println(vals)
+
+	// var str [5]string
+	// str[2] = "ok"
+	// fmt.Println(str)
+
+	//to declare in single line
+	// nums := [3]int{1, 2, 3}
+	// fmt.Println(nums)
+
+	//2d array
+	// var nums [2][2]int
+	// nums := [2][2]int{{3, 3}, {3, 3}}
+	// fmt.Println(nums)
+
+	//infer
+	// nums := [...]int{1, 2, 3}
+	var nums = [...]int{1, 2, 3}
+	fmt.Println(nums)
+
+}
